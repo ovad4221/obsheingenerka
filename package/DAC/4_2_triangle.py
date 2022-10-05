6337205f03a05cc5504d587a46a2ca6835a7fe3b
ovad4221 <andrey22ovs@yandex.ru>
2022-10-05 17:59:59 +0300

    ff2

--- package/DAC/4_2_triangle.py
+++ package/DAC/4_2_triangle.py
@@ -1,6 +1,22 @@
 from package.oop.oop import *
 from package.constants import *
+from time import sleep
 
 dac = init_pins(ports_dac)
 
+n = 0
+try:
+    t = int(input())
+    assert 0 < t < 1000
+    while True:
+        n = bin(n % 256)[2:].zfill(8)
+        binary_to_leds(dac, n)
+        n += 1
+        if n > 10 ** 10:
+            n = 0
 
+except Exception:
+    raise ValueError("error")
+
+finally:
+    all_off()
