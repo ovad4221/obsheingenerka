54b740f64c2f5725223f31b502300802189a2e8d
ovad4221 <andrey22ovs@yandex.ru>
2022-10-12 17:45:15 +0300

    Dac complited

--- package/DAC/4-3-pwm.py
+++ package/DAC/4-3-pwm.py
@@ -5,16 +5,24 @@ dac = init_pins(ports_dac)
 leds = init_pins(ports_leds)
 all_off(how='ld_pins', pins=dac + leds)
 dc = int(input()) % 100
-for i in range(len(leds)):
-    leds[i].pwm.start(100 - dc)
+for i in range(len(dac)):
+    dac[i].pwm.start(dc)
 old_dc = dc
+for i in range(len(leds)):
+    leds[i].pwm.start(dc)
 
 try:
     while True:
-        dc = int(input()) % 100
+        dc = int(input()) % 101
+        for i in range(len(dac)):
+            dac[i].change_dc(dc)
+            print(f"DAC current voltage {3.3 * dc / 100} V")
+
         for i in range(len(leds)):
-            leds[i].change_dc(100 - dc)
-            print(3.3 * dc / 100)
+            leds[i].change_dc(old_dc)
+            print(f"LEDs current voltage {3.3 * old_dc / 100} V")
+        old_dc = dc
+
 
 except Exception:
     raise ValueError("sth error")
