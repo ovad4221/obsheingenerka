4b3c1861d72251d0dfbd727d7f0dc405abe78bf0
ovad4221 <andrey22ovs@yandex.ru>
2022-10-19 18:17:57 +0300

    adc 2 norm

--- package/ADC/5-2-adc.py
+++ package/ADC/5-2-adc.py
@@ -8,23 +8,25 @@ troyka = Pin_Out(17, initial=True)
 
 try:
     while True:
-        binary = '10000000'
+        # 0 1 0 1 1 1 1 0
+        binary = [1, 0, 0, 0, 0, 0, 0, 0]
         for i in range(8):
-            binary_to_leds(dac, binary)
+            binary_to_leds(dac, ''.join([str(i) for i in binary]))
+            sleep(0.001)
             # comp.i_p True when V dac > V adc
             if i != 7:
                 if comp.is_power():
-                    binary = binary[:i] + '0' + binary[i + 1:]
+                    binary[i], binary[i + 1] = 0, 1
                 else:
-                    binary = binary[:i] + '1' + binary[i + 1:]
+                    binary[i], binary[i + 1] = 1, 1
+
             else:
                 if comp.is_power():
-                    binary = binary[:-1] + '0'
+                    binary[i] = 0
                 else:
-                    binary = binary[:-1] + '1'
-            sleep(0.01)
+                    binary[i] = 1
 
-        print(f"voltage DAC = {int(binary, 2) / 256 * 3.3} V")
+        print(f"voltage DAC = {int(''.join([str(i) for i in binary]), 2) / 256 * 3.3} V")
         sleep(2)
 
 except Exception as error:
