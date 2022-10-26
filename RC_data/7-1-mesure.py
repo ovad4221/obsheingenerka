894fa3e38757473600ed6df6c7372c680f488037
ovad4221 <andrey22ovs@yandex.ru>
2022-10-26 17:17:11 +0300

    RC_data 2.0

--- RC_data/7-1-mesure.py
+++ RC_data/7-1-mesure.py
@@ -2,8 +2,10 @@ from package.oop.oop import *
 from package.constants import *
 import csv
 import time
+from math import floor
 
 dac = init_pins(ports_dac)
+leds = init_pins(ports_leds)
 comp = Pin_In(4)
 troyka = Pin_Out(17)
 
@@ -21,13 +23,14 @@ def voltage():
                 binary[i], binary[i + 1] = 0, 1
             else:
                 binary[i], binary[i + 1] = 1, 1
-
         else:
             if comp.is_power():
                 binary[i] = 0
             else:
                 binary[i] = 1
 
+    st_one = '1' * floor(int(''.join([str(i) for i in binary]), 2) / 245 * 8)
+    binary_to_leds(leds, st_one.zfill(8))
     return int(''.join([str(i) for i in binary]), 2) / 256 * 3.3
 
 
@@ -38,11 +41,11 @@ with open('data_RC.csv', 'w') as file:
     time1 = time.time_ns()
     while True:
         v_new = voltage()
-        file.write(f"{time.time_ns() - time1},{round(v_new, 5)}")
+        file.write(f"{time.time_ns() - time1},{round(v_new, 5)}\n")
         if abs(v_new - v_old) < 0.01:
             if troyka.is_on:
                 troyka.off()
             else:
                 troyka.on()
         v_old = v_new
-        sleep(0.05)
+        sleep(0.01)
